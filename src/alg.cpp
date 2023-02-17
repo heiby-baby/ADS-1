cb1e38b87d058a0db467c925ca19c37d0d59629a
Кандрушин Максим 22-IST-1 <125025831+heiby-baby@users.noreply.github.com>
2023-02-17 15:55:45 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -2,19 +2,44 @@
 #include <cstdint>
 #include "alg.h"
 
-
 bool checkPrime(uint64_t value) {
-// вставьте код функции
+if (value == 2) return true;
+	if (value % 2 == 0) return false;
+	uint64_t b = value / 2 + 1;
+	for (uint64_t i = 3; i <= b; i = i + 2)
+	{
+		if (value % i == 0) return false;
+	}
+	return true;
 }
 
 uint64_t nPrime(uint64_t n) {
-// вставьте код функции
+if (n == 2) return 1;
+	uint64_t n_i = 1;					//n_i счётчик простых чисел
+	for (uint64_t x = 3;; x += 2)
+	{
+		if (checkPrime(x) == true) n_i++;
+		if (n_i == n) return x;
+	}
 }
 
 uint64_t nextPrime(uint64_t value) {
-// вставьте код функции
+for (uint64_t i = value + 1;; i++)
+	{
+		if (checkPrime(i) == true) return i;
+	}
 }
 
 uint64_t sumPrime(uint64_t hbound) {
-// вставьте код функции
+if (hbound < 3) return 0;
+	if (hbound == 3) return 2;
+	uint64_t sum = 2;
+	for (uint64_t i = 3; i < hbound; i += 2)
+	{
+		if (checkPrime(i) == true)
+		{
+			sum += i;
+		}
+	}
+	return sum;
 }
